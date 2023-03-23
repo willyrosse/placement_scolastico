4959833fd7360f43307402b9c43aeaac0586acb2
William Rossetti <rossetti.william.2004@gmail.com>
2023-03-23 21:08:49 +0100

    aggiornamento github

---
+++ Database_To_Invio.php
@@ -0,0 +1,113 @@
+<html>
+<head>
+    <title>Invio mail a destinatari</title>
+</head>
+</html>
+
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+// Connessione al database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lavoro";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$mail = new PHPMailer(true);
+
+$mail->SMTPOptions = array(
+    'ssl' => array(
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+    )
+    );
+
+try {
+    // Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'noreply.cannizzaro@gmail.com';         // SMTP username
+    $mail->Password   = 'dyhrddqzfcoikcqp';                      // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    // Recipients
+    $mail->setFrom('noreply.cannizzaro@gmail.com');
+
+    // Query per selezionare tutti i record dalla tabella subscribers
+
+    $sqlMail = "SELECT email FROM subscribers";
+    $result = mysqli_query($conn, $sqlMail);
+
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $to_address = $row["email"];
+            $mail->addAddress($to_address);
+        }
+    }
+      
+    // Query per selezionare tutti i record dalla tabella email_queue
+
+    $sql = "SELECT subject, message FROM email_queue";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $message="";
+        while($row = mysqli_fetch_assoc($result)) {
+
+            $subject = $row["subject"];
+            
+            $message .= "<br><br><br>" . "<strong>SUBJECT: " . $subject . "</strong> <br>BODY:". $row["message"];
+        }
+        
+            // Set recipient and content
+            $mail->addAddress($to_address);
+            $mail->isHTML(true);
+            $mail->Subject = "elenco mail di questa settimana";
+            $mail->Body = $message;
+
+            // invia mail ed elimina dal database
+            $mail->SMTPDebug = 0;
+            if ($mail->send()) {
+                $delete_sql = "DELETE FROM email_queue";
+                mysqli_query($conn, $delete_sql);
+            }
+
+// Clear recipients
+$mail->ClearAllRecipients();
+
+
+            // Clear recipients
+            $mail->ClearAllRecipients();
+        
+
+        // Stampa messaggio di avvenuto invio email
+        echo 'Tutte le email sono state inviate correttamente';
+    } else {
+        echo "Non ci sono email da inviare";
+    }
+
+    // Chiudi la connessione al database
+    
+    mysqli_close($conn);
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
--- subscriptionform.php
+++ subscriptionform.php
@@ -1,4 +1,23 @@
+<html>
+<head>
+<title>Registrazione Corso PCTO</title>
+</head>
+<body>
+<h1>Registrazione Corso PCTO</h1>
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <p>Email: <input type="email" name="email" size="40"></p>
+    <input type="submit" name="submit" value="Registrati">
+    <input type="submit" name="delete" value="Elimina">
+</form>
+</body>
+</html>
+
+
 <?php
+
+//con questo appaiono solo gli errori fatali e non i warning
+error_reporting(E_ERROR | E_PARSE);
+
 // Controllo se il form è stato inviato
 
 $email = $_POST["email"];
@@ -21,7 +40,7 @@ if (isset($_POST["submit"])) {
     }
 
     //controllo se l'email è già presente nella lista
-    $check_sql = "SELECT id FROM SUBSCRIBERS WHERE email = '".$email."'";
+    $check_sql = "SELECT id FROM subscribers WHERE email = '".$email."'";
     $check_res = $mysqli->query($check_sql);
     if ($check_res->num_rows > 0) {
         echo "Sei già registrato per questo corso!";
@@ -34,29 +53,35 @@ if (isset($_POST["submit"])) {
     if (!$add_res) {
         echo "Impossibile registrarsi per il corso. Riprova più tardi.";
         exit;
-        }
+    }
         
+    //chiusura della connessione a MySQL
+    $mysqli->close();
+        
+    echo "Sei ora registrato per il corso PCTO! Ti invieremo ulteriori informazioni alla tua email.";
+    exit;
+} elseif (isset($_POST["delete"])) {
+    // eliminazione dell'email dal database
 
-        //chiusura della connessione a MySQL
-        $mysqli->close();
-        
-        echo "Sei ora registrato per il corso PCTO! Ti invieremo ulteriori informazioni alla tua email.";
+    //connessione al database
+    $mysqli = new mysqli("localhost", "root", "", "lavoro");
+    if ($mysqli->connect_errno) {
+        echo "Connessione al MySQL fallita: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         exit;
     }
-        ?>
-        
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <title>Registrazione Corso PCTO</title>
-        </head>
-        <body>
-        <h1>Registrazione Corso PCTO</h1>
-        <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-            <p>Email: <input type="email" name="email" size="40"></p>
-            <input type="submit" name="submit" value="Registrati">
-        </form>
-        </body>
-        </html>
 
-        
+    //eliminazione dell'email dalla lista
+    $delete_sql = "DELETE FROM subscribers WHERE email = '".$email."'";
+    $delete_res = $mysqli->query($delete_sql);
+    if (!$delete_res) {
+        echo "Impossibile eliminare l'email. Riprova più tardi.";
+        exit;
+    }
+
+    //chiusura della connessione a MySQL
+    $mysqli->close();
+
+    echo "L'email ".$email." è stata eliminata dalla lista dei registrati al corso PCTO.";
+    exit;
+}
+?>
